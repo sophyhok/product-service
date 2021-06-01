5ccc17c83579735338457b1a2acd7415dc4f169e
Sophy Hok <sophy.hok@endurance.com>
2021-05-31 21:52:15 -0600

    creating service

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('hello from product service');
+})
+
+app.listen(4000, () => {
+  console.log('Product server running on port', 3000);
+})
