121b269d49f06f8b5c85264e7f22872bf6b50990
Sophy Hok <sophyhok90@gmail.com>
2021-05-31 22:04:10 -0600

    added /products

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 
 app.get('/hello', (req, res) => {
   res.send('hello from product service');
-})
+});
+
+app.get('/products', (req, res) => {
+  res.send('product lists');
+});
 
 app.listen(4000, () => {
   console.log('Product server running on port', 4000);
